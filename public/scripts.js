eed7ca84eb2c97107d4464bab2d6b428d6c797fc
Ryan Rozema <rrozema2@gmail.com>
2017-11-29 14:19:06 -0800

    Added more query functionality
    I added insert, delete, and update querys to the test database.  there needs to be some syntax changes but all in all it should be easy to convert that over to the final product

--- public/scripts.js
+++ public/scripts.js
@@ -56,3 +56,121 @@ for (var i = 0, l = elements.length; i < l; i++) {
     $search.attr('placeholder', (data[0].text !== '' ? data[0].text : $select.data('placeholder')));
   });
 }
+
+
+function sendInsertJSON(){
+
+  var http = new XMLHttpRequest();
+  var data = JSON.stringify([{
+        code: document.getElementById("code").value,
+        country_name: document.getElementById("name").value,
+        gdp: document.getElementById("gdp").value,
+        inflation: document.getElementById("inflation").value
+  }]);
+  console.log(data);
+  http.open("POST", "http://localhost:3000/loadTableData");
+
+
+  //Send the proper header information along with the request
+  http.setRequestHeader("Content-type", "application/json");
+
+  http.onreadystatechange = function() { // Call a function when the state changes.
+    if (http.readyState === 4 && http.status === 200) {
+    }
+  };
+  http.send(data);
+  document.location.reload();
+}
+
+function sendUpdateJSON(){
+
+  var http = new XMLHttpRequest();
+  var data = JSON.stringify({
+        code: document.getElementById("update-code").value,
+        country_name: document.getElementById("update-name").value,
+        gdp: document.getElementById("update-gdp").value,
+        inflation: document.getElementById("update-inflation").value
+  });
+  http.open("PUT", "http://localhost:3000/loadTableData");
+  console.log(data);
+
+  //Send the proper header information along with the request
+  http.setRequestHeader("Content-type", "application/json");
+
+  http.onreadystatechange = function() { // Call a function when the state changes.
+    if (http.readyState === 4 && http.status === 200) {
+    }
+  };
+  http.send(data);
+  document.location.reload();
+}
+
+
+function sendDeleteJSON(){
+
+  var http = new XMLHttpRequest();
+  var data = JSON.stringify({
+        code: document.getElementById("delete-code").value,
+  });
+  http.open("DELETE", "http://localhost:3000/loadTableData");
+  console.log(data);
+
+  //Send the proper header information along with the request
+  http.setRequestHeader("Content-type", "application/json");
+
+  http.onreadystatechange = function() { // Call a function when the state changes.
+    if (http.readyState === 4 && http.status === 200) {
+    }
+  };
+  http.send(data);
+  document.location.reload();
+}
+
+
+$(document).ready(function() {
+  var service = 'http://localhost:3000/';
+  jQuery.support.cors = true;
+  $.ajax({
+    type: "GET",
+    url: service + 'loadTableData',
+    contentType: "application/json; charset=utf-8",
+    success: function(data) {
+      console.log(data);
+      // EXTRACT VALUE FOR HTML HEADER.
+      var col = [];
+      for (var i = 0; i < data.length; i++) {
+        for (var key in data[i]) {
+          if (col.indexOf(key) === -1) {
+            col.push(key);
+          }
+        }
+      }
+
+      // CREATE DYNAMIC TABLE.
+      var table = document.getElementById("location");
+
+      // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+
+      var tr = table.insertRow(-1); // TABLE ROW.
+
+      for (i = 0; i < col.length; i++) {
+        var th = document.createElement("th"); // TABLE HEADER.
+        th.innerHTML = col[i];
+        tr.appendChild(th);
+      }
+
+      // ADD JSON DATA TO THE TABLE AS ROWS.
+      for (i = 0; i < data.length; i++) {
+        tr = table.insertRow(-1);
+        for (var j = 0; j < col.length; j++) {
+          var tabCell = tr.insertCell(-1);
+          tabCell.innerHTML = data[i][col[j]];
+        }
+      }
+    },
+
+    error: function(msg) {
+      alert(msg.responseText);
+    }
+  });
+})
--- server.js
+++ server.js
@@ -1,17 +1,27 @@
+// Dependencies
 var express = require('express');
 var mysql = require("mysql");
 var path = require('path');
-var tableify = require('tableify');
+var bodyParser = require('body-parser');
 
+
+// Our App
 var app = express();
 
+// Sets a static public directory for html, css, and java files
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
 
+
+// Routes
 app.get('/', onInitialLoad);
-
 app.get('/test', test);
 app.get('/loadTableData', loadTestTable);
+app.post('/loadTableData', insertTestTable);
+app.put('/loadTableData', updateTestTable);
+app.delete('/loadTableData', deleteFromTestTable);
 
+// Database connection info
 var connection = mysql.createConnection({
   host: "147.222.163.1",
   port: "3306",
@@ -19,7 +29,11 @@ var connection = mysql.createConnection({
   password: "rrozema69492485",
   database: "rrozema_DB"
 });
-  connection.connect();
+
+// Connect to the database
+connection.connect();
+
+// Functions for the route hits
 function onInitialLoad(request, response) {
   response.sendFile(path.join(__dirname + '/index.html'));
 }
@@ -30,34 +44,72 @@ function test(request, response) {
 }
 
 function loadTestTable(request, response) {
-    var result;
+  var result;
 
-    console.log("Database connection successful.");
+  console.log("Database connection successful.");
 
-    var queryString = 'SELECT * FROM Country;';
+  var queryString = "SELECT * FROM Country;";
 
-    connection.query(queryString, function(err, rows, fields) {
-      console.log(err);
-        if (err) throw err;
-        result = rows;
+  connection.query(queryString, function(err, rows, fields) {
+    if (err) throw err;
+    result = rows;
 
-        console.log("Database connection ended.");
-        response.send(result);
-        console.log("JSON data from the query has been sent.");
+    console.log("Database connection ended.");
+    response.send(result);
+    console.log("JSON data from the query has been sent.");
 
-    });
+  });
 }
 
+function insertTestTable(req, res) {
+  var jsondata = req.body;
+  console.log("json data is: " + JSON.stringify(jsondata));
+  var values = [];
+
+  for (var i = 0; i < jsondata.length; i++)
+    values.push([jsondata[i].code, jsondata[i].country_name, jsondata[i].gdp, jsondata[i].inflation]);
+
+  console.log(values);
+  console.log("values are " + values);
+  //Bulk insert using nested array [ [a,b],[c,d] ] will be flattened to (a,b),(c,d)
+  connection.query('INSERT INTO Country (code, country_name, gdp, inflation) VALUES ?;', [values], function(err, result) {
+    if (err) {
+      res.send(err.sqlMessage);
+    } else {
+      res.send('Success');
+    }
+  });
+}
+
+function updateTestTable(req, res) {
+  var jsondata = req.body;
+  console.log("json data is: " + jsondata);
+
+  //Bulk insert using nested array [ [a,b],[c,d] ] will be flattened to (a,b),(c,d)
+  connection.query('UPDATE Country SET country_name = ?, gdp = ?, inflation = ? WHERE code = ?;', [jsondata.country_name, jsondata.gdp, jsondata.inflation, jsondata.code], function(err, result) {
+    if (err) {
+      res.send(err.sqlMessage);
+    } else {
+      res.send('Success');
+    }
+  });
+}
+
+function deleteFromTestTable(req, res) {
+  var jsondata = req.body.code;
+  console.log("json data is: " + jsondata);
+
+  //Bulk insert using nested array [ [a,b],[c,d] ] will be flattened to (a,b),(c,d)
+  connection.query('DELETE FROM Country WHERE code = ?', jsondata, function(err, result) {
+    if (err) {
+      res.send(err.sqlMessage);
+    } else {
+      res.send('Success');
+    }
+  });
+}
+
+// Open up a port for our application
 app.listen(3000, function() {
   console.log("Our server is listening to localhost:3000");
 });
-
-/* Ending the connection on the database
-con.end(function(err) {
-  // The connection is terminated gracefully
-  // Ensures all previously enqueued queries are still
-  // before sending a COM_QUIT packet to the MySQL server.
-  if(err) console.log('err: ', err);
-  else console.log('Terminated done: ');
-});
-*/
